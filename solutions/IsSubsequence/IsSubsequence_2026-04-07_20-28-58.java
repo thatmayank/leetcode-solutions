f0ffa1661429fcca13d02b487c6cab75166a28fa
Mayank <63830975+thatmayank@users.noreply.github.com>
2026-04-07 20:28:59 +0530

    add: IsSubsequence_2026-04-07_20-28-58.java

---
+++ solutions/IsSubsequence/IsSubsequence_2026-04-07_20-28-58.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+
+        while(i < n && j < m) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else j++;
+        }
+
+        if(i == n) return true;
+        return false;
+    }
+}
